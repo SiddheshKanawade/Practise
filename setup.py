addc32b3f91ba06ad4b63889e6c031cca5ffb651
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-09-29 22:01:39 +0530

    add: initial configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+__version__='0.0.1'
+
+setup(
+    name='api-practice',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+    ]
+)
